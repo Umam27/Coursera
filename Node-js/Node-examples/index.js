524a6d2620fc10db6d38527615df8a03c4e7c027
Umam27 <umam20@iitk.ac.in>
2021-06-03 11:21:47 +0530

    getting started with node

---
+++ Node-js/Node-examples/index.js
@@ -0,0 +1,25 @@
+var arithmetic={
+    sum: (x,y)=> (x+y),
+    difference: (x,y)=> (x-y),
+    multiply: (x,y)=> (x*y),
+    divide: (x,y)=>(x/y)
+};
+
+function call_arithmetic(a,b){
+    console.log("We start here to solve with two number a = " + a + " , b = " + b);
+    console.log("The sum of the given numbers is "+ arithmetic.sum(a,b));
+    console.log("The difference of the given numbers is "+ arithmetic.difference(a,b));
+    console.log("The product of the given numbers is "+ arithmetic.multiply(a,b));
+    if(b<=0){
+        console.log("Division by zero and negative numbers are not allowed.");
+    }
+    else{
+        console.log("The qoutient of the given numbers is "+ arithmetic.divide(a,b));
+    }
+    console.log("\n");
+}
+
+
+call_arithmetic(4,5);
+call_arithmetic(3,-1);
+call_arithmetic(2,5);
